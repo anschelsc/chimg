7e0d1a65d0c81dfa48742bbadf401f5773eb5c95
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2023-08-08 15:35:50 -0600

    Actually upload the image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use anyhow::Result;
+use anyhow::{anyhow, bail, Result};
+use eggbug::Attachment;
 use serde::Deserialize;
 use std::path::Path;
+use std::env;
 
 fn main() {
-    if let Err(e) = test_chost() {
-        println!("{}", e);
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() != 2 {
+        eprintln!("Usage: {} FILENAME", args[0]);
+        return;
+    }
+    match upload(&args[1]) {
+        Ok(url) => println!("{}", url),
+        Err(e) => eprintln!("{}", e),
     }
 }
 
@@ -23,15 +31,43 @@ impl PageInfo {
 }
 
 #[tokio::main(flavor = "current_thread")]
-async fn test_chost() -> Result<()> {
+async fn upload<P: AsRef<Path>>(path: P) -> Result<String> {
     let info = PageInfo::load(".cohost.json")?;
     let session = eggbug::Session::login(&info.email, &info.password).await?;
+    let attachment = build_attachment(path).await?;
     let mut post = eggbug::Post {
-        headline: "Pest Toast".into(),
-        markdown: "plz do not gnore".into(),
+        markdown: "this draft can be deleted if you've used the image in another post".into(),
         draft: true,
+        attachments: vec![attachment],
         ..Default::default()
     };
     session.create_post(&info.page, &mut post).await?;
-    Ok(())
+    match post.attachments[0].url() {
+        None => Err(anyhow!("Failed to upload attachment")),
+        Some(url) => Ok(url.into()),
+    }
+}
+
+fn get_content_type<P: AsRef<Path>>(path: P) -> Option<String> {
+    Some(
+        match path.as_ref().extension()?.to_str()? {
+            "jpg" | "jpeg" => "image/jpeg",
+            "gif" => "image/gif",
+            "png" => "image/png",
+            "webp" => "image/webp",
+            "tiff" => "image/tiff",
+            _ => {
+                return None;
+            }
+        }
+        .into(),
+    )
+}
+
+async fn build_attachment<P: AsRef<Path>>(path: P) -> Result<Attachment> {
+    let content_type = match get_content_type(&path) {
+        Some(ct) => ct,
+        None => bail!("Unknown file extension"),
+    };
+    Ok(Attachment::new_from_file(path, content_type, None).await?)
 }
