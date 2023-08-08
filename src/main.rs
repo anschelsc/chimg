8d81624d153d30aaf117d8b81d1fba969132c587
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2023-08-08 13:50:55 -0600

    Initial commit (copied from chost)

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use serde::Deserialize;
+use std::path::Path;
+
+fn main() {
+    if let Err(e) = test_chost() {
+        println!("{}", e);
+    }
+}
+
+#[derive(Deserialize)]
+struct PageInfo {
+    email: String,
+    password: String,
+    page: String,
+}
+
+impl PageInfo {
+    fn load<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let raw = std::fs::read_to_string(path)?;
+        Ok(serde_json::from_str(&raw)?)
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn test_chost() -> Result<()> {
+    let info = PageInfo::load(".chost.json")?;
+    let session = eggbug::Session::login(&info.email, &info.password).await?;
+    let mut post = eggbug::Post {
+        headline: "Pest Toast".into(),
+        markdown: "plz do not gnore".into(),
+        draft: true,
+        ..Default::default()
+    };
+    session.create_post(&info.page, &mut post).await?;
+    Ok(())
+}
